1401a44085d9409f01af5c6075815cf5aeb37ab4
SirAdrian <radoiadrian77@gmail.com>
2020-01-07 17:16:17 +0200

    homework

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+class calculets {
+    public static void main(String[] args)
+    {
+
+        int n = 100;
+        int sum = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+
+}
