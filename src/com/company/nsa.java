0ad23c4b1cd541593533188c196592d3f4c98a78
SirAdrian <radoiadrian77@gmail.com>
2020-01-07 17:30:22 +0200

    home

--- src/com/company/nsa.java
+++ src/com/company/nsa.java
@@ -1,38 +1,30 @@
 package com.company;
 
-public class nsa {
+class Swichcasedenmo {
 
 
-    // Java program to print
-// all primes less than N
-    static class GFG
+
+    // Java program to illustrate switch-case
+    static class SwitchCaseDemo
     {
-        // function check whether
-// a number is prime or not
-        static boolean isPrime(int n)
+        public static void main(String args[])
         {
-// Corner case
-            if (n <= 1)
-                return false;
-
-// Check from 2 to n-1
-            for (int i = 2; i < n; i++)
-                if (n % i == 0)
-                    return false;
-
-            return true;
-        }
-
-        // Function to print primes
-        static void printPrime(int n)
-        {
-            for (int i = 2; i <= n; i++)
+            int i = 9;
+            switch (i)
             {
-                if (isPrime(i))
-                    System.out.print(i + " ");
+                case 0:
+                    System.out.println("i is zero.");
+                    break;
+                case 1:
+                    System.out.println("i is one.");
+                    break;
+                case 2:
+                    System.out.println("i is two.");
+                    break;
+                default:
+                    System.out.println("i is greater than 2.");
             }
         }
-
 
 
 
