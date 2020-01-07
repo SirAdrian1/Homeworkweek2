452a01607ab1b8fd9c6d3be63e5278c1fa05d298
SirAdrian <radoiadrian77@gmail.com>
2020-01-07 17:27:28 +0200

    home

---
+++ src/com/company/nsa.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class nsa {
+
+
+
+
+    
+    public static boolean isPalindrome(int number) {
+        int palindrome = number; // copied number into variable
+        int reverse = 0;
+
+        while (palindrome != 0) {
+            int remainder = palindrome % 10;
+            reverse = reverse * 10 + remainder;
+            palindrome = palindrome / 10;
+        }
+
+        // if original and reverse of number is equal means
+        // number is palindrome in Java
+        if (number == reverse) {
+            return true;
+        }
+        return false;
+    }
+}
