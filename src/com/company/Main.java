942cace6383267e81e101209216e3b441f14616a
SirAdrian <radoiadrian77@gmail.com>
2020-01-07 17:17:22 +0200

    homework

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,29 @@
 package com.company;
 
-class calculets {
-    public static void main(String[] args)
-    {
+import java.util.Scanner;
+class Main {
 
-        int n = 100;
-        int sum = 1;
-
-        for (int i = 1; i <= n; i++)
-        {
-            sum = sum + i;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int i, smallEven=0, bigOdd=0, countOdd=0, countEven=0;
+        for(i=0; i<n; i++){
+            System.out.print("Enter element "+(i+1));
+            a[i]=sc.nextInt();
+            if(a[i]%2 == 1){
+                if(countOdd>0 && a[i]>bigOdd) bigOdd=a[i];
+                else if(countOdd==0) bigOdd=a[i];
+                countOdd++;
+            }
+            else{
+                if(countEven>0 && a[i]<smallEven) smallEven=a[i];
+                else if(countEven==0)smallEven=a[i];
+                countEven++;
+            }
         }
-        System.out.println(sum);
+        System.out.println("The smallest even number is " + smallEven);
+        System.out.println("The largest odd number is " + bigOdd);
     }
-
 }
